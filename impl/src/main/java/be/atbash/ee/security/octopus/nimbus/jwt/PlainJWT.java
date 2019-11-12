b46068375429c630bc68f71682797fbdd3e21b46
Rudy De Busscher <rdebusscher@gmail.com>
2019-11-12 22:54:10 +0100

    Add PlainJWT code

---
+++ impl/src/main/java/be/atbash/ee/security/octopus/nimbus/jwt/PlainJWT.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2017-2019 Rudy De Busscher (https://www.atbash.be)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package be.atbash.ee.security.octopus.nimbus.jwt;
+
+
+import be.atbash.ee.security.octopus.nimbus.jose.JOSEObject;
+import be.atbash.ee.security.octopus.nimbus.jose.Payload;
+import be.atbash.ee.security.octopus.nimbus.jose.PlainHeader;
+import be.atbash.ee.security.octopus.nimbus.jose.PlainObject;
+import be.atbash.ee.security.octopus.nimbus.util.Base64URLValue;
+
+import javax.json.JsonObject;
+import java.text.ParseException;
+
+
+/**
+ * Unsecured (plain) JSON Web Token (JWT).
+ *
+ * @author Vladimir Dzhuvinov
+ * @version 2015-08-19
+ */
+public class PlainJWT extends PlainObject implements JWT {
+
+
+    private static final long serialVersionUID = 1L;
+
+
+    /**
+     * Creates a new unsecured (plain) JSON Web Token (JWT) with a default
+     * {@link be.atbash.ee.security.octopus.nimbus.jose.PlainHeader} and the specified claims
+     * set.
+     *
+     * @param claimsSet The JWT claims set. Must not be {@code null}.
+     */
+    public PlainJWT(JWTClaimsSet claimsSet) {
+
+        super(new Payload(claimsSet.toJSONObject()));
+    }
+
+
+    /**
+     * Creates a new unsecured (plain) JSON Web Token (JWT) with the
+     * specified header and claims set.
+     *
+     * @param header    The unsecured header. Must not be {@code null}.
+     * @param claimsSet The JWT claims set. Must not be {@code null}.
+     */
+    public PlainJWT(PlainHeader header, JWTClaimsSet claimsSet) {
+
+        super(header, new Payload(claimsSet.toJSONObject()));
+    }
+
+
+    /**
+     * Creates a new unsecured (plain) JSON Web Token (JWT) with the
+     * specified Base64URL-encoded parts.
+     *
+     * @param firstPart  The first part, corresponding to the unsecured
+     *                   header. Must not be {@code null}.
+     * @param secondPart The second part, corresponding to the claims set
+     *                   (payload). Must not be {@code null}.
+     * @throws ParseException If parsing of the serialised parts failed.
+     */
+    public PlainJWT(Base64URLValue firstPart, Base64URLValue secondPart)
+            throws ParseException {
+
+        super(firstPart, secondPart);
+    }
+
+
+    @Override
+    public JWTClaimsSet getJWTClaimsSet()
+            throws ParseException {
+
+        JsonObject json = getPayload().toJSONObject();
+
+        if (json == null) {
+
+            throw new ParseException("Payload of unsecured JOSE object is not a valid JSON object", 0);
+        }
+
+        return JWTClaimsSet.parse(json);
+    }
+
+
+    /**
+     * Parses an unsecured (plain) JSON Web Token (JWT) from the specified
+     * string in compact format.
+     *
+     * @param s The string to parse. Must not be {@code null}.
+     * @return The unsecured JWT.
+     * @throws ParseException If the string couldn't be parsed to a valid
+     *                        unsecured JWT.
+     */
+    public static PlainJWT parse(final String s)
+            throws ParseException {
+
+        Base64URLValue[] parts = JOSEObject.split(s);
+
+        if (!parts[2].toString().isEmpty()) {
+
+            throw new ParseException("Unexpected third Base64URL part in the unsecured JWT object", 0);
+        }
+
+        return new PlainJWT(parts[0], parts[1]);
+    }
+}
---
+++ impl/src/test/java/be/atbash/ee/security/octopus/nimbus/jwt/PlainJWTTest.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2017-2019 Rudy De Busscher (https://www.atbash.be)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package be.atbash.ee.security.octopus.nimbus.jwt;
+
+
+import be.atbash.ee.security.octopus.nimbus.jose.Algorithm;
+import be.atbash.ee.security.octopus.nimbus.jose.JOSEObjectType;
+import be.atbash.ee.security.octopus.nimbus.jose.PlainHeader;
+import be.atbash.ee.security.octopus.nimbus.util.Base64URLValue;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlainJWTTest {
+
+    @Test
+    public void testClaimsSetConstructor()
+            throws Exception {
+
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject("alice")
+                .issuer("http://c2id.com")
+                .audience("http://app.example.com")
+                .build();
+
+        JWTClaimsSet readOnlyClaimsSet = claimsSet;
+
+        PlainJWT jwt = new PlainJWT(readOnlyClaimsSet);
+
+        assertThat(jwt.getJWTClaimsSet().getSubject()).isEqualTo("alice");
+        assertThat(jwt.getJWTClaimsSet().getIssuer()).isEqualTo("http://c2id.com");
+        assertThat(jwt.getJWTClaimsSet().getAudience().get(0)).isEqualTo("http://app.example.com");
+    }
+
+    @Test
+    public void testHeaderAndClaimsSetConstructor()
+            throws Exception {
+
+        PlainHeader header = new PlainHeader.Builder().customParam("exp", 1000L).build();
+
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject("alice")
+                .issuer("http://c2id.com")
+                .audience("http://app.example.com")
+                .build();
+
+        JWTClaimsSet readOnlyClaimsSet = claimsSet;
+
+        PlainJWT jwt = new PlainJWT(header, readOnlyClaimsSet);
+
+        assertThat(jwt.getHeader()).isEqualTo(header);
+
+        assertThat(jwt.getJWTClaimsSet().getSubject()).isEqualTo("alice");
+        assertThat(jwt.getJWTClaimsSet().getIssuer()).isEqualTo("http://c2id.com");
+        assertThat(jwt.getJWTClaimsSet().getAudience().get(0)).isEqualTo("http://app.example.com");
+    }
+
+    @Test
+    public void testBase64URLConstructor()
+            throws Exception {
+
+        // {"alg":"none"}
+        Base64URLValue part1 = new Base64URLValue("eyJhbGciOiJub25lIn0");
+
+        // {"iss":"joe","exp":1300819380,"http://example.com/is_root":true}
+        Base64URLValue part2 = new Base64URLValue("eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFt" +
+                "cGxlLmNvbS9pc19yb290Ijp0cnVlfQ");
+
+        PlainJWT jwt = new PlainJWT(part1, part2);
+
+        assertThat(jwt.getHeader().getAlgorithm()).isEqualTo(Algorithm.NONE);
+        assertThat(jwt.getHeader().getType()).isNull();
+        assertThat(jwt.getHeader().getContentType()).isNull();
+
+        JWTClaimsSet cs = jwt.getJWTClaimsSet();
+
+        assertThat(cs.getIssuer()).isEqualTo("joe");
+        assertThat(cs.getExpirationTime()).isEqualTo(new Date(1300819380L * 1000));
+        assertThat((Boolean) cs.getClaim("http://example.com/is_root")).isTrue();
+    }
+
+    @Test
+    public void testParse()
+            throws Exception {
+
+        String s = "eyJhbGciOiJub25lIn0" +
+                "." +
+                "eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFt" +
+                "cGxlLmNvbS9pc19yb290Ijp0cnVlfQ" +
+                ".";
+
+        PlainJWT jwt = PlainJWT.parse(s);
+
+        assertThat(jwt.getHeader().getAlgorithm()).isEqualTo(Algorithm.NONE);
+        assertThat(jwt.getHeader().getType()).isNull();
+        assertThat(jwt.getHeader().getContentType()).isNull();
+
+        JWTClaimsSet cs = jwt.getJWTClaimsSet();
+
+        assertThat(cs.getIssuer()).isEqualTo("joe");
+        assertThat(cs.getExpirationTime()).isEqualTo(new Date(1300819380L * 1000));
+        assertThat((Boolean) cs.getClaim("http://example.com/is_root")).isTrue();
+    }
+
+    @Test
+    public void testExampleKristina()
+            throws Exception {
+
+        String jwtString = "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0=\n" +
+                ".eyJleHAiOjM3NzQ4NjQwNSwiYXpwIjoiRFAwMWd5M1Frd1ZHR2RJZWpJSmdMWEN0UlRnYSIsInN1\n" +
+                "YiI6ImFkbWluQGNhcmJvbi5zdXBlciIsImF1ZCI6IkRQMDFneTNRa3dWR0dkSWVqSUpnTFhDdFJU\n" +
+                "Z2EiLCJpc3MiOiJodHRwczpcL1wvbG9jYWxob3N0Ojk0NDNcL29hdXRoMmVuZHBvaW50c1wvdG9r\n" +
+                "ZW4iLCJpYXQiOjM3Mzg4NjQwNX0=\n" +
+                ".";
+
+        PlainJWT plainJWT = PlainJWT.parse(jwtString);
+
+        // Header
+        assertThat(plainJWT.getHeader().getAlgorithm()).isEqualTo(Algorithm.NONE);
+        assertThat(plainJWT.getHeader().getType()).isEqualTo(new JOSEObjectType("JWT"));
+
+        // Claims
+        assertThat(plainJWT.getJWTClaimsSet().getExpirationTime()).isEqualTo(new Date(377486405L * 1000));
+        assertThat(plainJWT.getJWTClaimsSet().getClaim("azp")).isEqualTo("DP01gy3QkwVGGdIejIJgLXCtRTga");
+        assertThat(plainJWT.getJWTClaimsSet().getSubject()).isEqualTo("admin@carbon.super");
+        assertThat(plainJWT.getJWTClaimsSet().getAudience().get(0)).isEqualTo("DP01gy3QkwVGGdIejIJgLXCtRTga");
+        assertThat(plainJWT.getJWTClaimsSet().getIssuer()).isEqualTo("https://localhost:9443/oauth2endpoints/token");
+        assertThat(plainJWT.getJWTClaimsSet().getIssueTime()).isEqualTo(new Date(373886405L * 1000));
+    }
+
+    @Test
+    public void testTrimWhitespace()
+            throws Exception {
+
+        PlainJWT jwt = new PlainJWT(new JWTClaimsSet.Builder().build());
+        String jwtString = " " + jwt.serialize() + " ";
+        PlainJWT.parse(jwtString);
+    }
+}
