910564736c9b3985e11c7094f6f5822eeb3cbe8c
Rudy De Busscher <rdebusscher@gmail.com>
2020-01-04 19:14:33 +0100

    Use ECNamedCurveSpec for creating EC keys within JWK.

--- impl/src/main/java/be/atbash/ee/security/octopus/keys/AtbashKey.java
+++ impl/src/main/java/be/atbash/ee/security/octopus/keys/AtbashKey.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 Rudy De Busscher (https://www.atbash.be)
+ * Copyright 2017-2020 Rudy De Busscher (https://www.atbash.be)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ package be.atbash.ee.security.octopus.keys;
 
 import be.atbash.ee.security.octopus.keys.selector.AsymmetricPart;
 import be.atbash.ee.security.octopus.keys.selector.SecretKeyType;
+import be.atbash.ee.security.octopus.nimbus.jwk.Curve;
 import be.atbash.ee.security.octopus.nimbus.jwk.KeyType;
 import be.atbash.util.PublicAPI;
 import be.atbash.util.resource.ResourceUtil;
@@ -62,7 +63,8 @@ public class AtbashKey {
     public String getSpecification() {
         StringBuilder result = new StringBuilder();
         if (KeyType.EC.equals(secretKeyType.getKeyType())) {
-            result.append("Curve name : ").append(ECCurveHelper.getCurve((ECKey) key).getName());
+            Curve curve = ECCurveHelper.getCurve((ECKey) key);
+            result.append("Curve name : ").append(curve == null ? "unknown" : curve.getName());
         }
         if (KeyType.RSA.equals(secretKeyType.getKeyType())) {
             RSAKey rsaKey = (RSAKey) key;
