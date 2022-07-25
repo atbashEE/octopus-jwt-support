f3ab6fd75b7500cf5c1236951fd4a458c3c17ddf
Rudy De Busscher <rdebusscher@gmail.com>
2022-07-25 19:36:17 +0200

    Possibility to periodically recreate SecureRandom instance and use developer provided instance.
    (cherry picked from commit b7adb7203e8e28bd0a624edeecb5e7de513bcb7c)

--- impl/src/main/java/be/atbash/ee/security/octopus/config/JCASupportConfiguration.java
+++ impl/src/main/java/be/atbash/ee/security/octopus/config/JCASupportConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 Rudy De Busscher (https://www.atbash.be)
+ * Copyright 2017-2022 Rudy De Busscher (https://www.atbash.be)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,9 +24,14 @@ import be.atbash.config.logging.StartupLogging;
 import be.atbash.ee.security.octopus.nimbus.jose.crypto.bc.BouncyCastleProviderSingleton;
 import be.atbash.util.StringUtils;
 import be.atbash.util.reflection.CDICheck;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.ServiceLoader;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  *
@@ -36,9 +41,13 @@ import java.security.SecureRandom;
 //  Java Cryptography Architecture
 public class JCASupportConfiguration extends AbstractConfiguration implements ModuleConfig {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(JCASupportConfiguration.class);
+
     private static final Object SECURE_RANDOM_LOCK = new Object();
 
     private SecureRandom secureRandom;
+
+    private TimerTask task;
 
     @ConfigEntry
     public String getSecureRandomAlgorithm() {
@@ -49,22 +58,54 @@ public class JCASupportConfiguration extends AbstractConfiguration implements Mo
     public SecureRandom getSecureRandom() {
         synchronized (SECURE_RANDOM_LOCK) {
             if (secureRandom == null) {
-                String algo = getSecureRandomAlgorithm();
-                if (StringUtils.isEmpty(algo)) {
-                    secureRandom = new SecureRandom();
-                } else {
-                    try {
-                        secureRandom = SecureRandom.getInstance(algo, BouncyCastleProviderSingleton.getInstance());
-                    } catch (NoSuchAlgorithmException e) {
-                       throw new ConfigurationException(e.getMessage());
+                tryServiceLoader();
+                if (secureRandom == null) {
+                    String algo = getSecureRandomAlgorithm();
+                    if (StringUtils.isEmpty(algo)) {
+                        secureRandom = new SecureRandom();
+                    } else {
+                        try {
+                            secureRandom = SecureRandom.getInstance(algo, BouncyCastleProviderSingleton.getInstance());
+                        } catch (NoSuchAlgorithmException e) {
+                            throw new ConfigurationException(e.getMessage());
+                        }
                     }
                 }
             }
-            // FIXME Do we need a Thread which does the reseed every now and then
-            //secureRandom.setSeed(secureRandom.generateSeed());
+            // Do we need to recreate the Secure Random after x seconds of usage?
+            Integer secureRandomRecreateSeconds = getSecureRandomRecreateSeconds();
+            if (secureRandomRecreateSeconds != 0 && task == null) {
+                long recreatePeriod = secureRandomRecreateSeconds * 1000;
+                task = new TimerTask() {
+                    public void run() {
+                        synchronized (SECURE_RANDOM_LOCK) {
+                            // reset variable so that SecureRandom is recreated next time.
+                            secureRandom = null;
+                        }
+                    }
+                };
+                new Timer("Task to recreate SecureRandom").scheduleAtFixedRate(task, recreatePeriod, recreatePeriod);
+            }
         }
         return secureRandom;
     }
+
+    private void tryServiceLoader() {
+        ServiceLoader<SecureRandomProvider> loader = ServiceLoader.load(SecureRandomProvider.class);
+        for (SecureRandomProvider provider : loader) {
+            if (secureRandom != null) {
+                LOGGER.warn("Service loader for 'SecureRandomProvider' returned multiple providers. The selected provider may be different next time.");
+                break;
+            }
+            secureRandom = provider.get();
+        }
+    }
+
+    @ConfigEntry
+    public Integer getSecureRandomRecreateSeconds() {
+        return getOptionalValue("jwt.jca.securerandom.recreate", 0, Integer.class);
+    }
+
 
     // Java SE Support
     private static JCASupportConfiguration INSTANCE;
--- impl/src/test/java/be/atbash/ee/security/octopus/config/JCASupportConfigurationTest.java
+++ impl/src/test/java/be/atbash/ee/security/octopus/config/JCASupportConfigurationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 Rudy De Busscher (https://www.atbash.be)
+ * Copyright 2017-2022 Rudy De Busscher (https://www.atbash.be)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,45 +18,122 @@ package be.atbash.ee.security.octopus.config;
 import be.atbash.config.exception.ConfigurationException;
 import be.atbash.config.test.TestConfig;
 import be.atbash.util.TestReflectionUtils;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
 
 import java.security.SecureRandom;
+import java.util.List;
+import java.util.TimerTask;
+import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+// Difficult to clean up TimerTask and Thread that goes with it. So execute test methods in certain order.
 class JCASupportConfigurationTest {
+
+    static boolean useProvider = false;
 
     @AfterEach
     public void cleanup() throws NoSuchFieldException {
         TestConfig.resetConfig();
+
+        // Stop the Task; but this doesn't mean the Thread is immediately gone. Not even when we do Thread.sleep for a while.
+        Object task = TestReflectionUtils.getValueOf(JCASupportConfiguration.getInstance(), "task");
+        if (task != null) {
+            TimerTask timerTask = (TimerTask) task;
+            timerTask.cancel();
+        }
+
         // Reset the static variable to allow static to be initialized again.
         TestReflectionUtils.resetOf(JCASupportConfiguration.class, "INSTANCE");
+
+        useProvider = false;
     }
 
 
     @Test
+    @Order(1)
     void getSecureRandom_default() {
         SecureRandom secureRandom = JCASupportConfiguration.getInstance().getSecureRandom();
-        assertThat(secureRandom).isNotNull();
+        Assertions.assertThat(secureRandom).isNotNull();
+
+        // Check if by default no task is created to recreate SecureRandom
+        List<String> threadNames = Thread.getAllStackTraces().keySet()
+                .stream()
+                .map(Thread::getName)
+                .collect(Collectors.toList());
+        Assertions.assertThat(threadNames).doesNotContain("Task to recreate SecureRandom");
+
     }
 
     @Test
+    @Order(2)
     void getSecureRandom_configured() {
         TestConfig.addConfigValue("jwt.jca.securerandom.algo", "NONCEANDIV");
 
         SecureRandom secureRandom = JCASupportConfiguration.getInstance().getSecureRandom();
-        assertThat(secureRandom).isNotNull();
-        assertThat(secureRandom.getAlgorithm()).isEqualTo("NONCEANDIV");
+        Assertions.assertThat(secureRandom).isNotNull();
+        Assertions.assertThat(secureRandom.getAlgorithm()).isEqualTo("NONCEANDIV");
     }
 
     @Test
+    @Order(3)
     void getSecureRandom_invalid() {
         TestConfig.addConfigValue("jwt.jca.securerandom.algo", "xyz");
 
-        ConfigurationException exception = Assertions.assertThrows(ConfigurationException.class, () -> JCASupportConfiguration.getInstance().getSecureRandom());
-        assertThat(exception.getMessage()).isEqualTo("no such algorithm: xyz for provider BC");
+        Assertions.assertThatThrownBy(() -> JCASupportConfiguration.getInstance().getSecureRandom())
+                .isInstanceOf(ConfigurationException.class)
+                .hasMessage("no such algorithm: xyz for provider BC");
 
+    }
+
+    @Test
+    @Order(4)
+    void getSecureRandom_recreate() throws InterruptedException {
+        // 1 second recreate
+        TestConfig.addConfigValue("jwt.jca.securerandom.recreate", "1");
+
+        SecureRandom secureRandom1 = JCASupportConfiguration.getInstance().getSecureRandom();
+        Assertions.assertThat(secureRandom1).isNotNull();
+
+        SecureRandom secureRandom2 = JCASupportConfiguration.getInstance().getSecureRandom();
+        Assertions.assertThat(secureRandom2).isSameAs(secureRandom1);
+
+        Thread.sleep(1100);  // We should have done the recreate by now
+
+        SecureRandom secureRandom3 = JCASupportConfiguration.getInstance().getSecureRandom();
+        Assertions.assertThat(secureRandom3).isNotSameAs(secureRandom1);
+
+        List<String> threadNames = Thread.getAllStackTraces().keySet()
+                .stream()
+                .map(Thread::getName)
+                .collect(Collectors.toList());
+        Assertions.assertThat(threadNames).contains("Task to recreate SecureRandom");
+
+    }
+
+    @Test
+    @Order(5)
+    void getSecureRandom_provider() {
+        useProvider = true;
+        SecureRandom secureRandom = JCASupportConfiguration.getInstance().getSecureRandom();
+        Assertions.assertThat(secureRandom).isNotNull();
+
+        Assertions.assertThat(secureRandom).isSameAs(TestSecureRandomProvider.secureRandomByProvider);
+
+    }
+
+    public static class TestSecureRandomProvider implements SecureRandomProvider {
+
+        static SecureRandom secureRandomByProvider;
+
+        @Override
+        public SecureRandom get() {
+            secureRandomByProvider = null;
+            if (useProvider) {
+                secureRandomByProvider = new SecureRandom();
+                return secureRandomByProvider;
+            }
+            return null;
+        }
     }
 }
